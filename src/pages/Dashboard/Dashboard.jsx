4d62a6ce6c621d9d079d692b9333b0198c3a6d23
Guilherme H. <ghantani@gmail.com>
2025-02-19 22:02:23 -0300

    Update: Dashboard
    Show products from API in a table

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from './Dashboard.module.css'
+import { ProductContext } from '../../context/ProductContext'
+import {Cell, Column, Row, Table, TableBody, TableHeader} from 'react-aria-components';
 
 function Dashboard() {
+  const {products} = useContext(ProductContext);
+
   return (
-    <h1>Dashboard</h1>
+    <section>
+      <Table className={styles.table} aria-label="Files" selectionMode="multiple">
+        <TableHeader className={styles.table__header}>
+          <Column isRowHeader className={styles.header__text}>Id</Column>
+          <Column className={styles.header__text}>Name</Column>
+          <Column isRowHeader className={styles.header__text}>Image</Column>
+          <Column className={styles.header__text}>Price</Column>
+          <Column className={styles.header__text}>Category</Column>
+          <Column className={styles.header__text}>Quantity</Column>
+          <Column className={styles.header__text}></Column>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => {return(
+            <Row className={styles.row}>
+              <Cell>{product.id}</Cell>
+              <Cell>{product.name}</Cell>
+              <Cell><img src={product.image} height={96} width={96}/></Cell>
+              <Cell>{`R$${product.price}`}</Cell>
+              <Cell>{product.category}</Cell>
+              <Cell>{product.quantity}</Cell>
+              <Cell>
+                <div className={styles.edit__del}>
+                  <button className={styles.icon__button}><img src='/images/edit.svg' height={30}/></button>
+                  <button className={styles.icon__button}><img src='/images/delete.svg' height={30}/></button>
+                </div>
+              </Cell>
+            </Row>)
+          })}
+        </TableBody>
+      </Table>
+    </section>
   )
 }
 
