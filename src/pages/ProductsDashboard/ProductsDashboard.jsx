c4d17fa904ed522199436402bed2ec34bed4c94e
Guilherme H. <ghantani@gmail.com>
2025-06-23 10:22:43 -0300

    Add: Reponsiveness to Dashboards

--- src/pages/ProductsDashboard/ProductsDashboard.jsx
+++ src/pages/ProductsDashboard/ProductsDashboard.jsx
@@ -30,10 +30,10 @@ function ProductsDashboard() {
             <TableHeader className={styles.table__header}>
               <Column isRowHeader className={styles.header__text}>Id</Column>
               <Column className={styles.header__text}>Name</Column>
-              <Column isRowHeader className={styles.header__text}>Image</Column>
-              <Column className={styles.header__text}>Price</Column>
-              <Column className={styles.header__text}>Category</Column>
-              <Column className={styles.header__text}>Quantity</Column>
+              <Column isRowHeader className={`${styles.header__text} ${styles.optional2}`}>Image</Column>
+              <Column className={`${styles.header__text} ${styles.optional}`}>Price</Column>
+              <Column className={`${styles.header__text} ${styles.optional}`}>Category</Column>
+              <Column className={`${styles.header__text} ${styles.optional}`}>Quantity</Column>
               <Column className={styles.header__text}></Column>
             </TableHeader>
             <TableBody className={styles.table__body}>
@@ -42,10 +42,10 @@ function ProductsDashboard() {
                   <Row className={styles.row}>
                     <Cell>{product.id}</Cell>
                     <Cell>{product.name}</Cell>
-                    <Cell><img src={product.image} height={96} width={96}/></Cell>
-                    <Cell>{`R$${product.price}`}</Cell>
-                    <Cell>{product.category}</Cell>
-                    <Cell>{product.quantity}</Cell>
+                    <Cell className={styles.optional2}><img src={product.image} height={96} width={96}/></Cell>
+                    <Cell className={styles.optional}>{`R$${product.price}`}</Cell>
+                    <Cell className={styles.optional}>{product.category}</Cell>
+                    <Cell className={styles.optional}>{product.quantity}</Cell>
                     <Cell>
                       <div className={styles.edit__del}>
                         <NavLink className={styles.icon__button} to={`/productsDashboard/productEditForm/${product.id}`}><img src='/images/edit.svg' height={30}/></NavLink>
